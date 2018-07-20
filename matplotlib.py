155a14a8207f047ba8286ee6ea67def64c8e9653
Best Kang <41457596+dasekang@users.noreply.github.com>
2018-07-20 11:08:28 +0900

    Add files via upload

---
+++ matplotlib.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+a = tf.random_normal([2,20])
+sess = tf.Session()
+out=sess.run(a)
+x, y = out
+
+plt.scatter(x,y)
+plt.show()
